010d45a291871b476c0c47567a5aebebe72189f3
unknown <saikiran.com>
2023-07-20 16:43:22 +0530

    java file commited

---
+++ first.java
@@ -0,0 +1,21 @@
+class Ameer
+{
+	int fact(int a){}
+	void max(int a int b, int c){}
+    int get(){}
+	void show(){}
+	int add(int a, int b){}
+	void cube(inta){}
+	public static void main(String[] args) 
+	{
+		system.out.println("method ....");
+		Ameer d=new Ameer();
+		new Ameer().d fact(5);
+		new Ameer().d max(5,4,8);
+	    new Ameer().d get();
+		new Ameer().d show();
+		new Ameer().d add(7,5);
+	    new Ameer().d cube(8);
+	}
+}
+
